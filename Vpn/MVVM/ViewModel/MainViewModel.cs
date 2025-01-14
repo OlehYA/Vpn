5358a48651f4f38947d6b7f9b8f7be20e42ee20d
OlehYa <olehyakuts@gmail.com>
2025-01-14 22:27:53 +0100

    Create a Main View Model

---
+++ Vpn/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Vpn.Core;
+
+namespace Vpn.MVVM.ViewModel
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand MoveWindowCommand { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView 
+        { 
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            Application.Current.MainWindow.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+
+            MoveWindowCommand = new RelayCommand(o => { Application.Current.MainWindow.DragMove(); });
+        }
+    }
+}
