c9d188fe324e05263445b36c8745380f70d64ce8
OlehYa <olehyakuts@gmail.com>
2025-01-15 20:49:38 +0100

    Create an Observable Collection

--- Vpn/MVVM/ViewModel/ProtectionViewModel.cs
+++ Vpn/MVVM/ViewModel/ProtectionViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,19 @@ namespace Vpn.MVVM.ViewModel
 {
     internal class ProtectionViewModel
     {
+        public ObservableCollection<ServerModel> Servers { get; set; }
+
         public ProtectionViewModel() 
         {
+            Servers= new ObservableCollection<ServerModel>();
+            for (int i = 0; i < 10; i++)
+            {
+                Servers.Add(new ServerModel
+                {
+                    Country ="USA"
+                });
 
+            }
         }
     }
 }
